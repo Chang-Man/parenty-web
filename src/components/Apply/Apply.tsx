b50ee4703bb35acc07a5865b42b75904aa9b7f18
Chang-Man <changagim@snu.ac.kr>
2022-05-11 00:51:59 +0900

    icon

--- src/components/Apply/Apply.tsx
+++ src/components/Apply/Apply.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styles from './Apply.module.scss';
 
 const Apply = () => {
-  return <div>바이</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <span className={styles.big}>[접수 모집 안내]</span>
+        <br />
+        모집 기간: 5월 12일(목)~22일(금)
+        <br />
+        수강 기간: 5월 23일(월)~6월 20일(월)
+        <br />
+        20명 초과 시, 추첨을 통해 선발합니다.
+        <br />
+        <br />
+        <button className={styles.link}>신청 링크 연결</button>
+      </div>
+    </div>
+  );
 };
 
 export default Apply;
