921d005b3a980b518e5397c20a3c6a5fcfb3310d
Chang-Man <changagim@snu.ac.kr>
2022-05-18 12:55:55 +0900

    수정

--- src/components/Apply/Apply.tsx
+++ src/components/Apply/Apply.tsx
@@ -7,9 +7,9 @@ const Apply = () => {
       <div className={styles.container}>
         <span className={styles.big}>[접수 모집 안내]</span>
         <br />
-        모집 기간: 5월 12일(목)~22일(금)
+        모집 기간: 5월 12일(목)-29일(일)
         <br />
-        수강 기간: 5월 23일(월)~6월 20일(월)
+        수강 기간: 6월 13일(월)-7월 10일(일)
         <br />
         20명 초과 시, 추첨을 통해 선발합니다.
         <br />
